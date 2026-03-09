1d7623eca4d49fdb1aadb10858ee58fb6d980d4e
matthewhogan22 <matthewhogan2203@gmail.com>
2026-03-09 19:53:12 -0400

    Finished basic functionality. Ready for initial upload

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 "use strict";
 
-const to_do_list = [];
+var to_do_list = [];
 
 function validate_to_do(added_todo_text, added_todo_title) {
     return added_todo_title.length > 0 && added_todo_text.length > 0;
 
 }
 
-function add_todo_visual(item) {
-    $(".to-do-items").append(`
-    <div class="to-do-item">
-        <div class="to-do-item-header">
-            <input class="to-do-checkbox" type="checkbox">
-            <label class="to-do-checkbox-label">${item.title}</label>
-        </div>
-        <p class="to-do-description">${item.text}</p>
-    </div>
-`);
+function render_to_do_list() {
+    const container = $(".to-do-items");
+    container.empty();
+
+    to_do_list.forEach(function(item) {
+        container.append(`
+            <div class="to-do-item" data-id="${item.id}">
+                <div class="to-do-item-header">
+                    <input class="to-do-checkbox" type="checkbox">
+                    <label class="to-do-checkbox-label">${item.title}</label>
+                </div>
+                <p class="to-do-description">${item.text}</p>
+            </div>
+        `);
+    });
 }
 
 jQuery(async function($) {
@@ -38,18 +43,29 @@ jQuery(async function($) {
         var title = $("#todo-title").val();
         var text = $("#todo-description").val();
         if (validate_to_do(title, text)) {
-            var item = {title: title, text: text, done: false};
+            var item = {id: Date.now(), title: title, text: text, done: false};
             to_do_list.push(item);
             $("#add-modal").hide();
             $("#todo-title").val("");
             $("#todo-description").val("");
-            add_todo_visual(item);
+            render_to_do_list();
         }
     });
 
     var update_list = $("#update-button");
     update_list.on("click", async function(e) {
-        console.log(to_do_list);
+        const checked_ids = [];
+
+        $(".to-do-checkbox:checked").each(function() {
+            const id = $(this).closest(".to-do-item").data("id");
+            checked_ids.push(id);
+        });
+
+        to_do_list = to_do_list.filter(function(item) {
+            return !checked_ids.includes(item.id);
+        });
+
+        render_to_do_list();
     });
 
 
