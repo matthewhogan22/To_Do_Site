66c6bf86fcfa02a507de70202941bd6380954430
matthewhogan22 <matthewhogan2203@gmail.com>
2026-03-25 12:28:55 -0400

    Formatting calendar header to be responsive

--- backend/static/calendar.js
+++ backend/static/calendar.js
@@ -18,6 +18,34 @@ function getNumDaysInMonth(month) {
     return 28
 }
 
+function getMonthString(month) {
+    if (month == 0) {
+        return "January"
+    } else if (month == 1) {
+        return "February"
+    } else if (month == 2) {
+        return "March"
+    } else if (month == 3) {
+        return "April"
+    } else if (month == 4) {
+        return "May"
+    } else if (month == 5) {
+        return "June"
+    } else if (month == 6) {
+        return "July"
+    } else if (month == 7) {
+        return "August"
+    } else if (month == 8) {
+        return "September"
+    } else if (month == 9) {
+        return "October"
+    } else if (month == 10) {
+        return "November"
+    } else {
+        return "December"
+    }
+}
+
 function format_calendar(startDay) {
     clear_calendar()
     for (let i = 0; i < getNumDaysInMonth(month); i++) {
@@ -28,7 +56,10 @@ function format_calendar(startDay) {
                                     </p>
                                 </div>`
                     
-        }
+    }
+    const headerP = document.getElementById("month-head");
+    var headerText = getMonthString(month) + " " + year;
+    headerP.textContent = headerText;
 }
 
 function clear_calendar() {
