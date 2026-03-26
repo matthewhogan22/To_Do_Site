b63460dd82f8c1e7883e8ab6e0efdab527f31547
matthewhogan22 <matthewhogan2203@gmail.com>
2026-03-26 14:39:15 -0400

    Bug fix load calendar

--- backend/static/calendar.js
+++ backend/static/calendar.js
@@ -108,6 +108,7 @@ jQuery(async function($) {
         var newFirstDayOfMonth = new Date(year, month, 1);
         var newDayOfWeekOfMonth = newFirstDayOfMonth.getDay();
         format_calendar(newDayOfWeekOfMonth + 1)
+        console.log(to_do_list)
     });
 
 
